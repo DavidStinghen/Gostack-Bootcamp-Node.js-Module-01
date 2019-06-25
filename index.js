dd271188bef64ea19390e608051ecca0a1c25915
DavidNatanael <davidstinghen@gmail.com>
2019-06-25 11:20:34 -0300

    fet: end crud methods

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const express = require('express')
 const server = express()
 
+// server can read json files
+server.use(express.json())
+
 /**
  * query params = ?teste=1
  * route params = /test/1
@@ -27,11 +30,56 @@ server.get('/test/:id', (req, res) => {
 const users = ['David', 'Diego', 'Julio'] 
 
 /**
+ * list route
+ * list all users
+ */
+server.get('/users', (req, res) => {
+    return res.json(users)
+})
+
+/**
  * params route
+ * get the users by index 
  */
 server.get('/users/:index', (req,res) => {
     const { index } = req.params
     return res.json(users[ index ])
+})
+
+/**
+ * post route
+ * store new users
+ */
+server.post('/users', (req, res) => {
+    const { name } = req.body
+    users.push(name)
+
+    return res.json(users)
+})
+
+/**
+ * put route
+ * update user by index param
+ * change the name in the body
+ */
+server.put('/users/:index', (req, res) => {
+    const { index } = req.params
+    const { name } = req.body
+
+    users[index] = name
+
+    return res.json(users)
+})
+
+/**
+ * delete route
+ * get user by index
+ */
+server.delete('/users/:index', (req, res) => {
+    const { index } = req.params
+    users.splice(index, 1)
+
+    return res.send()
 })
 
 // server listen port 3000
