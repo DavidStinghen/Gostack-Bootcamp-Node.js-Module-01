dd7e5f39da30ba3d35475b5a7f366a0d4c286ea1
DavidNatanael <davidstinghen@gmail.com>
2019-06-25 10:33:04 -0300

    fet: query and route params

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const express = require('express')
 const server = express()
 
 /**
+ * query params = ?teste=1
+ * route params = /test/1
+ * request body =  { "nome": "test", "email": "test@test.com.br"}
+ */
+
+/**
  * first route - method GET
  */
-server.get('/test', (req, res) => {
+server.get('/test/:id', (req, res) => {
     // console.log('test')
-   // return res.send('Hello Word!')
-   return res.json({ message: 'Hello Word' })
+    // return res.send('Hello Word!')
+    // return res.json({ message: 'Hello Word' })
+
+    //const name =  req.query.name
+    // return res.json({ message: `Hello ${name}` })
+
+    const { id } = req.params
+    return res.json({ message: `Searching test ${id}`})
 })
 
 // server listen port 3000
